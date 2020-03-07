4ed6f194ccb15441897893478335f3166781c14f
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2020-03-07 23:46:37 +0000

    enable CUA mode

--- spacemacs/personal-config.el
+++ spacemacs/personal-config.el
@@ -1,3 +1,10 @@
+(cua-mode t)
+(setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+(transient-mark-mode 1) ;; No region when it is not highlighted
+(setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+(global-set-key (kbd "C-z") 'undo)
+
+
 (defun count-occurences (regex string)
   (recursive-count regex string 0))
 (defun recursive-count (regex string start)
