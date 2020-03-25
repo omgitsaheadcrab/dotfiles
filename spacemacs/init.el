3d440996bf86d91a4c9c326043e71a6a004f887b
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2020-03-25 23:46:53 +0000

    add xclipboard for terminal support

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -61,6 +61,7 @@ values."
      shell-scripts
      sql
      button-lock
+     xclipboard
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
