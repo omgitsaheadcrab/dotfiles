7ffb17cdbc5f7dfd08a9a8afa3299ebed5e54bbc
Tobias Backer Dirks <omgitsaheadcrab@gmail.com>
2022-10-16 07:57:54 +0300

    revert: unfk merge

--- polybar/run_polybar.sh
+++ polybar/run_polybar.sh
@@ -7,9 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch top and bottom
-FC_DEBUG=0 polybar -c ~/.config/polybar/simple-black top-left &
-FC_DEBUG=0 polybar -c ~/.config/polybar/simple-black bottom-left &
-FC_DEBUG=0 polybar -c ~/.config/polybar/simple-black top-right &
-FC_DEBUG=0 polybar -c ~/.config/polybar/simple-black bottom-right &
+polybar -c ~/.config/polybar/simple-black top &
+polybar -c ~/.config/polybar/simple-black bottom &
 
 echo "Bars launched..."
